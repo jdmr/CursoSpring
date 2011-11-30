d1b26beb80f4ed6d84d857e5adcfd1e44222bc1e
J. David Mendoza <jdmendoza@um.edu.mx>
2011-11-30 13:04:46 -0600

    Agregamos AOP al proyecto

---
+++ EjercicioSpring05/src/main/java/escuela/InterceptaAlumno.java
@@ -0,0 +1,14 @@
+package escuela;
+
+import java.util.Date;
+
+/**
+ *
+ * @author jdmr
+ */
+public class InterceptaAlumno {
+
+    public void intercepta() {
+        System.out.println("Se esta agregando un alumno a las "+new Date());
+    }
+}
