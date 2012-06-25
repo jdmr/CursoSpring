91b29f00357c647f8e39e3a78b04847360a47d8a
J. David Mendoza <jdmendoza@um.edu.mx>
2012-06-25 11:06:47 -0500

    Clase en la UM

---
+++ EjercicioSpring07/src/main/java/escuela/App.java
@@ -0,0 +1,41 @@
+package escuela;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.List;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        ApplicationContext context = new ClassPathXmlApplicationContext("escuela.xml");
+        
+        AlumnoDao alumnoDao = context.getBean(AlumnoDao.class);
+        
+        NumberFormat nf = DecimalFormat.getInstance();
+        NumberFormat nf2 = DecimalFormat.getInstance();
+        
+        nf.setMinimumIntegerDigits(4);
+        nf.setGroupingUsed(false);
+        nf2.setMinimumIntegerDigits(2);
+        for(int i = 1; i <= 10; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Alumno").append(nf2.format(i));
+            Alumno alumno = new Alumno(nf.format(i), sb.toString(), sb.toString());
+            
+            alumnoDao.crea(alumno);
+        }
+        
+        List<Alumno> alumnos = alumnoDao.lista();
+        for(Alumno alumno : alumnos) {
+            System.out.println(alumno);
+        }
+    }
+}
